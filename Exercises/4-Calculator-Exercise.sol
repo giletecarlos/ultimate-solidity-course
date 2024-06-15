a35f2602528b5b82e1b705d07e36c1b57ba38f5b
giletecarlos <giletecarlos@gmail.com>
2024-06-15 13:00:42 +0200

    Add Calculator contract

--- Exercises/4-Calculator-Exercise.sol
+++ Exercises/4-Calculator-Exercise.sol
@@ -8,6 +8,25 @@ pragma solidity ^0.8.0;
 // 4️⃣ Create a function to get result
 
 contract Calculator {
-    uint256 public result;
+    uint256 public result = 0;
 
+    function add(uint256 num) public {
+        result += num;
+    }
+
+    function substract(uint256 num) public {
+        result -= num;
+    }
+
+    function multiply(uint256 num) public {
+        result *= num;
+    }
+
+    function divide(uint256 num) public {
+        result /= num;
+    }
+
+    function get() public view returns (uint256) {
+        return result;
+    }
 }
