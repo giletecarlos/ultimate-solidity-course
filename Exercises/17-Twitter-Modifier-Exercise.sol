9a7de547cad2df661936c8a98695a473c47a974b
Carlos Gilete <giletecarlos@gmail.com>
2024-06-18 13:24:18 +0200

    feat: Add owner and max tweet length to Twitter contract

--- Exercises/17-Twitter-Modifier-Exercise.sol
+++ Exercises/17-Twitter-Modifier-Exercise.sol
@@ -2,7 +2,7 @@
 
 // 1️⃣ Add a function called changeTweetLength to change max tweet length
 // HINT: use newTweetLength as input for function
-// 2️⃣ Create a constructor function to set an owner of contract
+// 2️⃣ Create a constructor function to set an owner of contract ✅
 // 3️⃣ Create a modifier called onlyOwner
 // 4️⃣ Use onlyOwner on the changeTweetLength function
 
@@ -10,7 +10,8 @@ pragma solidity ^0.8.0;
 
 contract Twitter {
 
-    uint16 constant MAX_TWEET_LENGTH = 280;
+    address owner;
+    uint16 max_tweet_lenght;
 
     struct Tweet {
         address author;
@@ -18,10 +19,24 @@ contract Twitter {
         uint256 timestamp;
         uint256 likes;
     }
+
     mapping(address => Tweet[] ) public tweets;
 
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not the owner");
+        _;
+    }
+
+    function changeTweetLenght(uint16 newTweetLenght) public onlyOwner(){
+        max_tweet_lenght = newTweetLenght;
+    }
+ 
     function createTweet(string memory _tweet) public {
-        require(bytes(_tweet).length <= MAX_TWEET_LENGTH, "Tweet is too long bro!" );
+        require(bytes(_tweet).length <= max_tweet_lenght, "Tweet is too long bro!" );
 
         Tweet memory newTweet = Tweet({
             author: msg.sender,
